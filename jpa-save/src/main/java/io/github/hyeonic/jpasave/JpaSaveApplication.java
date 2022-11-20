fd6f6d9a178458c18d316a2c1194dc14b3656566
hyeonic <dev.hyeonic@gmail.com>
2022-11-20 21:35:23 +0900

    feat: init jpa-save in blog-code

---
+++ jpa-save/src/main/java/io/github/hyeonic/jpasave/JpaSaveApplication.java
@@ -0,0 +1,13 @@
+package io.github.hyeonic.jpasave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaSaveApplication.class, args);
+    }
+
+}
---
+++ jpa-save/src/test/java/io/github/hyeonic/jpasave/JpaSaveApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hyeonic.jpasave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
