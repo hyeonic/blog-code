b7fe4e095e448411eb23f5388950aa0742c48dac
hyeonic <dev.hyeonic@gmail.com>
2022-11-30 16:15:08 +0900

    feat: init optimistic-locking project

---
+++ optimistic-locking/src/main/java/io/github/hyeonic/optimisticlocking/OptimisticLockingApplication.java
@@ -0,0 +1,13 @@
+package io.github.hyeonic.optimisticlocking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimisticLockingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptimisticLockingApplication.class, args);
+    }
+
+}
---
+++ optimistic-locking/src/test/java/io/github/hyeonic/optimisticlocking/OptimisticLockingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hyeonic.optimisticlocking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimisticLockingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
