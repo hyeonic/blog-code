b789df5f99dc3c9f0c1d8279bc401b6bfc9029eb
hyeonic <dev.hyeonic@gmail.com>
2022-12-09 17:50:23 +0900

    feat: init spring-transaction project

---
+++ spring-transaction/src/test/java/io/github/hyeonic/springtransaction/SpringTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hyeonic.springtransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTransactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
