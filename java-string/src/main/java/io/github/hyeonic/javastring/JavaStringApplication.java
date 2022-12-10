2ae2ebe63be0bb396f45800ed68a52a3dab4981c
hyeonic <dev.hyeonic@gmail.com>
2022-12-10 13:37:44 +0900

    feat: init java-string project

---
+++ java-string/src/main/java/io/github/hyeonic/javastring/JavaStringApplication.java
@@ -0,0 +1,13 @@
+package io.github.hyeonic.javastring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaStringApplication.class, args);
+    }
+
+}
---
+++ java-string/src/test/java/io/github/hyeonic/javastring/JavaStringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hyeonic.javastring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
