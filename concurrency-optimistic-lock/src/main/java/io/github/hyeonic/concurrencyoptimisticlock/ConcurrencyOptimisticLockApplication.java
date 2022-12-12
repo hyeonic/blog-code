801c4c200e3a528eb2c5b2630aaf9c30b030c3e3
hyeonic <dev.hyeonic@gmail.com>
2022-12-12 18:49:52 +0900

    feat: init concurrency-optimistic-lock project

---
+++ concurrency-optimistic-lock/src/main/java/io/github/hyeonic/concurrencyoptimisticlock/ConcurrencyOptimisticLockApplication.java
@@ -0,0 +1,13 @@
+package io.github.hyeonic.concurrencyoptimisticlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyOptimisticLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrencyOptimisticLockApplication.class, args);
+    }
+
+}
---
+++ concurrency-optimistic-lock/src/test/java/io/github/hyeonic/concurrencyoptimisticlock/ConcurrencyOptimisticLockApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hyeonic.concurrencyoptimisticlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyOptimisticLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
