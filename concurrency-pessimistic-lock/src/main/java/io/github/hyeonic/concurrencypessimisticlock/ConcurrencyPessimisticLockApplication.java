1e79674bd934300209b2311b828046486d36a695
hyeonic <dev.hyeonic@gmail.com>
2022-12-12 13:50:18 +0900

    feat: Add concurrency-pessimistic-lock project

---
+++ concurrency-pessimistic-lock/src/main/java/io/github/hyeonic/concurrencypessimisticlock/ConcurrencyPessimisticLockApplication.java
@@ -0,0 +1,13 @@
+package io.github.hyeonic.concurrencypessimisticlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyPessimisticLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrencyPessimisticLockApplication.class, args);
+    }
+
+}
---
+++ concurrency-pessimistic-lock/src/test/java/io/github/hyeonic/concurrencypessimisticlock/ConcurrencyPessimisticLockApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hyeonic.concurrencypessimisticlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyPessimisticLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
