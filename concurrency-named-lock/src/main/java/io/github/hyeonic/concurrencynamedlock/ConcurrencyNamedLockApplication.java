436bd30e1ad1a4b700ea3f6915a4b12f9cb0e732
hyeonic <dev.hyeonic@gmail.com>
2022-12-14 12:27:37 +0900

    feat: init concurrency-named-lock

---
+++ concurrency-named-lock/src/main/java/io/github/hyeonic/concurrencynamedlock/ConcurrencyNamedLockApplication.java
@@ -0,0 +1,13 @@
+package io.github.hyeonic.concurrencynamedlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyNamedLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrencyNamedLockApplication.class, args);
+    }
+
+}
---
+++ concurrency-named-lock/src/test/java/io/github/hyeonic/concurrencynamedlock/ConcurrencyNamedLockApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hyeonic.concurrencynamedlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyNamedLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
