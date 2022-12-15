18487f6a0ffaf9bbe2ba358ea58a656147521182
hyeonic <dev.hyeonic@gmail.com>
2022-12-15 22:44:10 +0900

    feat: init concurrency-spin-lock

---
+++ concurrency-spin-lock/src/test/java/io/github/hyeonic/concurrencyspinlock/ConcurrencySpinLockApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hyeonic.concurrencyspinlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencySpinLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
