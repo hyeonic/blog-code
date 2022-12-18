20a9ffb2e68c590ea462a6fd569a7ed0d9584b1e
hyeonic <dev.hyeonic@gmail.com>
2022-12-18 22:29:15 +0900

    feat: init query-performance-detector project

---
+++ query-performance-detector/README.md
@@ -0,0 +1,1 @@
+# query-performance-detector
---
+++ query-performance-detector/src/main/java/io/github/hyeonic/queryperformancedetector/QueryPerformanceDetectorApplication.java
@@ -0,0 +1,13 @@
+package io.github.hyeonic.queryperformancedetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryPerformanceDetectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueryPerformanceDetectorApplication.class, args);
+    }
+
+}
