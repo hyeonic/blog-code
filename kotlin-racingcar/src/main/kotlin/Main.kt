a6849c61d77ca8e2b70e33b70253d2bf90bf13e4
hyeonic <dev.hyeonic@gmail.com>
2023-01-10 16:40:40 +0900

    feat: 자동차 이름 입력 기능 추가

--- kotlin-racingcar/src/main/kotlin/Main.kt
+++ kotlin-racingcar/src/main/kotlin/Main.kt
@@ -1,5 +1,11 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.util.*
 
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+
+    println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+    val input = scanner.nextLine()
+    val carNames = input.split(",")
+
+    println(carNames)
 }
