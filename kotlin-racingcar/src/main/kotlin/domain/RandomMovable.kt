6876e06477f303ce2e807c973fdb30644fa5d71b
hyeonic <dev.hyeonic@gmail.com>
2023-01-13 13:14:57 +0900

    refactor: 상수 추출

--- kotlin-racingcar/src/main/kotlin/domain/RandomMovable.kt
+++ kotlin-racingcar/src/main/kotlin/domain/RandomMovable.kt
@@ -2,8 +2,13 @@ package domain
 
 class RandomMovable : Movable {
 
+    companion object {
+        private const val MIN_NUMBER = 1
+        private const val MAX_NUMBER = 9
+        private const val STANDARD_NUMBER = 4
+    }
+
     override fun isMove(): Boolean {
-        val number = (1..9).random()
-        return number >= 4
+        return (MIN_NUMBER..MAX_NUMBER).random() >= STANDARD_NUMBER
     }
 }
