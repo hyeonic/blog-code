2107fc0e647c90ef9dcb0a18191c684ae0cbd8a2
hyeonic <dev.hyeonic@gmail.com>
2023-01-14 13:14:43 +0900

    feat: 재입력 로직 추가

--- kotlin-racingcar/src/main/kotlin/Main.kt
+++ kotlin-racingcar/src/main/kotlin/Main.kt
@@ -1,17 +1,15 @@
 import domain.*
+import java.lang.RuntimeException
 import java.util.*
 
 fun main(args: Array<String>) {
     val scanner = Scanner(System.`in`)
 
     println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
-    val input = scanner.nextLine()
-    val carNames = input.split(",")
-
-    val cars = Cars(carNames, RandomMovable())
+    val cars = retryInputCarNames(scanner)
 
     println("시도할 회수는 몇회인가요?")
-    val count = Count(scanner.nextLine())
+    val count = retryInputCount(scanner)
 
     println("\n실행 결과")
     while (!count.isZero()) {
@@ -35,4 +33,20 @@ private fun printCar(cars: Cars) {
     }
 
     println()
+}
+
+private fun retryInputCarNames(scanner: Scanner): Cars = try {
+    val input = scanner.nextLine()
+    val carNames = input.split(",")
+    Cars(carNames, RandomMovable())
+} catch (e: RuntimeException) {
+    println(e.message)
+    retryInputCarNames(scanner)
+}
+
+private fun retryInputCount(scanner: Scanner): Count = try {
+    Count(scanner.nextLine())
+} catch (e: RuntimeException) {
+    println(e.message)
+    retryInputCount(scanner)
 }
