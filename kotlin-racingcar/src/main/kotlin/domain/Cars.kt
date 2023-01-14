7239a3ace6fbe79bd63ca407ba2df04d5558b4be
hyeonic <dev.hyeonic@gmail.com>
2023-01-14 12:00:08 +0900

    feat: 차 이동 및 우승자 추출 기능 추가

--- kotlin-racingcar/src/main/kotlin/domain/Cars.kt
+++ kotlin-racingcar/src/main/kotlin/domain/Cars.kt
@@ -1,8 +1,12 @@
 package domain
 
 class Cars(
-    val value: List<Car>,
+    value: List<String>,
+    private val movable: Movable
 ) {
+
+    val value: List<Car> = value.map(::CarName)
+        .map(::Car)
 
     init {
         validateDuplicatedCarName()
@@ -14,8 +18,20 @@ class Cars(
         }
     }
 
-    private fun toNoneDuplicatedCarNames() = value
-        .map { it.carName }
-        .map { it.value }
+    private fun toNoneDuplicatedCarNames() = value.map(Car::carName)
+        .map(CarName::value)
         .toSet()
+
+    fun moveAll() {
+        for (car in value) {
+            car.moveOrStop(movable)
+        }
+    }
+
+    fun extractWinner(): List<Car> {
+        val maxCar = value.maxBy(Car::position)
+        val maxCarPosition = maxCar.position
+
+        return value.filter { maxCarPosition == it.position }
+    }
 }
--- kotlin-racingcar/src/test/kotlin/domain/CarsTest.kt
+++ kotlin-racingcar/src/test/kotlin/domain/CarsTest.kt
@@ -8,21 +8,30 @@ class CarsTest {
 
     @Test
     fun `자동차 목록 일급 컬렉션을 생성한다`() {
-        val cars = listOf("pat", "mat")
-            .map { CarName(it) }
-            .map { Car(it) }
+        val carNames = listOf("pat", "mat")
 
-        val actual = Cars(cars)
+        val actual = Cars(carNames, RandomMovable())
 
         assertEquals(actual.value.size, 2)
     }
 
     @Test
     fun `자동차 목록 중 이름이 중복되는 경우 예외를 던진다`() {
-        val cars = listOf("mat", "mat")
-            .map { CarName(it) }
-            .map { Car(it) }
+        val carNames = listOf("mat", "mat")
 
-        assertThrows<IllegalArgumentException> { Cars(cars) }
+        assertThrows<IllegalArgumentException> { Cars(carNames, RandomMovable()) }
+    }
+
+    @Test
+    fun `자동차 목록 중 우승자를 출력한다`() {
+        val carNames = listOf("mat", "pat")
+        val movable = { true }
+
+        val cars = Cars(carNames, movable)
+        cars.moveAll()
+
+        val actual = cars.extractWinner()
+
+        assertEquals(actual.size, 2)
     }
 }
