0dcb22724e6ca7c4bd7c69c12644a1868b6eb262
hyeonic <dev.hyeonic@gmail.com>
2023-03-04 22:55:50 +0900

    refactor: ThreadLocal nullable 허용

--- jpa-auditing/src/main/kotlin/hyeoni/c/jpaauditing/common/RequestContext.kt
+++ jpa-auditing/src/main/kotlin/hyeoni/c/jpaauditing/common/RequestContext.kt
@@ -3,10 +3,10 @@ package hyeoni.c.jpaauditing.common
 class RequestContext {
 
     companion object {
-        private val currentAuditorStore = ThreadLocal<String>()
+        private val currentAuditorStore = ThreadLocal<String?>()
 
         var currentAuditor: String
-            get() = currentAuditorStore.get()
+            get() = currentAuditorStore.get() ?: "default"
             set(currentAuditor) {
                 currentAuditorStore.remove()
                 return currentAuditorStore.set(currentAuditor)
