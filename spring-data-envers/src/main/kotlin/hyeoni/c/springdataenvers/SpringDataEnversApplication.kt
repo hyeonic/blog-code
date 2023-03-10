c05ae7dd1067332cd33f4291709b2a1cf1c25adf
hyeonic <dev.hyeonic@gmail.com>
2023-03-10 21:10:03 +0900

    feat: init spring-data-envers repository

---
+++ spring-data-envers/src/main/kotlin/hyeoni/c/springdataenvers/SpringDataEnversApplication.kt
@@ -0,0 +1,11 @@
+package hyeoni.c.springdataenvers
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringDataEnversApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringDataEnversApplication>(*args)
+}
---
+++ spring-data-envers/src/test/kotlin/hyeoni/c/springdataenvers/SpringDataEnversApplicationTests.kt
@@ -0,0 +1,13 @@
+package hyeoni.c.springdataenvers
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringDataEnversApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
