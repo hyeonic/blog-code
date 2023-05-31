002dfd6b3139d93cd83cfecd4328f6a69e96c93c
hyeonic <dev.hyeonic@gmail.com>
2023-05-31 23:11:18 +0900

    feat: init transactional-with-interface repository

---
+++ transactional-with-interface/src/main/kotlin/c/hyeonic/transactionalwithinterface/TransactionalWithInterfaceApplication.kt
@@ -0,0 +1,11 @@
+package c.hyeonic.transactionalwithinterface
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TransactionalWithInterfaceApplication
+
+fun main(args: Array<String>) {
+    runApplication<TransactionalWithInterfaceApplication>(*args)
+}
---
+++ transactional-with-interface/src/test/kotlin/c/hyeonic/transactionalwithinterface/TransactionalWithInterfaceApplicationTests.kt
@@ -0,0 +1,13 @@
+package c.hyeonic.transactionalwithinterface
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TransactionalWithInterfaceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
