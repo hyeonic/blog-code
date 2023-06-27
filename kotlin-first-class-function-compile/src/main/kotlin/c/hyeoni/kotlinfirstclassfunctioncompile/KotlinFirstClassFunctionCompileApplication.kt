e44b2bfcfe0803fbccfb234b4808af2db0f2739c
hyeonic <dev.hyeonic@gmail.com>
2023-06-27 23:37:19 +0900

    feat: init kotlin-first-class-function-complie repository

---
+++ kotlin-first-class-function-compile/src/main/kotlin/c/hyeoni/kotlinfirstclassfunctioncompile/KotlinFirstClassFunctionCompileApplication.kt
@@ -0,0 +1,11 @@
+package c.hyeoni.kotlinfirstclassfunctioncompile
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinFirstClassFunctionCompileApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinFirstClassFunctionCompileApplication>(*args)
+}
---
+++ kotlin-first-class-function-compile/src/test/kotlin/c/hyeoni/kotlinfirstclassfunctioncompile/KotlinFirstClassFunctionCompileApplicationTests.kt
@@ -0,0 +1,13 @@
+package c.hyeoni.kotlinfirstclassfunctioncompile
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinFirstClassFunctionCompileApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
